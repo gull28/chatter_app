23b7bfdeec4749827793e3ab570bacb57a71e18c
krisjanis.straupe <krisjanis.straupe@dataflow.lv>
2023-02-05 22:00:14 +0200

    apollo graphql integration

--- App.js
+++ App.js
@@ -4,28 +4,38 @@ import React from 'react';
 import {LandingPage} from './imports/client/LandingPage/LandingPage';
 import {LoginScreen} from './imports/client/LoginScreen/LoginScreen';
 import {RegisterScreen} from './imports/client/RegisterScreen/RegisterScreen';
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+
 const Stack = createNativeStackNavigator();
+
+const client = new ApolloClient({
+  uri: 'https://countries.trevorblades.com/graphql',
+  cache: new InMemoryCache(),
+});
+
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerBackVisible: false}}>
-        <Stack.Screen
-          name="LandingPage"
-          component={LandingPage}
-          options={{
-            headerLeft: () => null,
-          }}
-        />
-        <Stack.Screen
-          name="LoginScreen"
-          component={LoginScreen}
-          options={{
-            headerLeft: () => null,
-          }}
-        />
-        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerBackVisible: false}}>
+          <Stack.Screen
+            name="LandingPage"
+            component={LandingPage}
+            options={{
+              headerLeft: () => null,
+            }}
+          />
+          <Stack.Screen
+            name="LoginScreen"
+            component={LoginScreen}
+            options={{
+              headerLeft: () => null,
+            }}
+          />
+          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ApolloProvider>
   );
 };
 export default App;
